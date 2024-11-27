fee401b9bd4f005bc6a85c41a4f9c4b94d8c2157
ChristinaBustos <105693387+ChristinaBustos@users.noreply.github.com>
2024-11-27 13:37:23 -0600

    [UPDATE]

--- gpds-ui/src/views/Home.vue
+++ gpds-ui/src/views/Home.vue
@@ -2,7 +2,7 @@
 <template>
     <div>
       <h1>Home</h1>
-      <p>Bienvenido a la página de inicio.Hola jenkins</p>
+      <p>Bienvenido a la página de inicio.Hola jenkins,porfiin</p>
     </div>
   </template>
   
